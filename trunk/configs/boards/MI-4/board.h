4a8e3770e6e4d24ee1a372ec981e0e706e16a1e7
jie1jie <91038725+jie1jie@users.noreply.github.com>
2024-02-16 14:19:45 +0800

    Add files via upload

--- trunk/configs/boards/MI-4/board.h
+++ trunk/configs/boards/MI-4/board.h
@@ -1,27 +1,26 @@
-/* Xiaomi Mi-4 */
+/* Xiaomi MI 4C (R4CL)*/
 
-#define BOARD_PID		"MI-4"
-#define BOARD_NAME		"MI-4"
-#define BOARD_DESC		"Xiaomi Mi 4 Wireless Router"
+#define BOARD_PID		"MI-4C"
+#define BOARD_NAME		"MI-4C"
+#define BOARD_DESC		"Xiaomi MI 4C Wireless Router"
 #define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
-#define BOARD_VENDOR_URL	"https://www.mi.com/"
-#define BOARD_MODEL_URL		"https://www.mi.com/miwifi4"
-#define BOARD_BOOT_TIME		20
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/miwifilite"
+#define BOARD_BOOT_TIME		25
 #define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	18
-#undef  BOARD_GPIO_BTN_WPS
-#undef  BOARD_GPIO_LED_ALL
-#undef  BOARD_GPIO_LED_WIFI
-#define BOARD_GPIO_LED_POWER	8	/* 6: red, 10: yellow, 8: blue */
-#define BOARD_GPIO_LED_LAN	10
-#undef  BOARD_GPIO_LED_WAN
-#undef  BOARD_GPIO_LED_ROUTER
-#define BOARD_HAS_5G_11AC	1
-#define BOARD_NUM_ANT_5G_TX	2
-#define BOARD_NUM_ANT_5G_RX	2
+#undef BOARD_GPIO_BTN_RESET	
+#define  BOARD_GPIO_BTN_WPS     38
+#undef  BOARD_GPIO_LED_ALL 
+#define BOARD_GPIO_LED_WIFI	26       /*11  */
+#define BOARD_GPIO_LED_POWER	11	/* 24: blue, 26: yellow, 29: red */
+#undef  BOARD_GPIO_LED_LAN     29
+#define  BOARD_GPIO_LED_WAN     24     /*  */
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
 #define BOARD_NUM_ANT_2G_TX	2
 #define BOARD_NUM_ANT_2G_RX	2
 #define BOARD_NUM_ETH_LEDS	1
 #define BOARD_NUM_ETH_EPHY	3
-#define BOARD_HAS_EPHY_L1000	1
-#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
